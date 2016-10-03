1b3c2273619332392fcb02def2f84c2bb7624975
eyalhen100 <eyalhen100@gmail.com>
2016-10-03 09:22:00 +0300

    added search. using observable

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { HttpModule }    from '@angular/http';
+import './rxjs-extensions';
+
 
 // Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
@@ -13,11 +15,12 @@ import { HeroesComponent } from './heroes.component';
 import { DashboardComponent } from './dashboard.component';
 import { HeroService } from './hero.service';
 import { routing } from './app.routing';
+import { HeroSearchComponent } from './hero-search.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, HttpModule, 
   					InMemoryWebApiModule.forRoot(InMemoryDataService), routing],
-  declarations: [ AppComponent, DashboardComponent, HeroDetailComponent, HeroesComponent],
+  declarations: [ AppComponent, DashboardComponent, HeroDetailComponent, HeroesComponent, HeroSearchComponent],
   providers: 	[ HeroService],
   bootstrap:    [ AppComponent]
 })
